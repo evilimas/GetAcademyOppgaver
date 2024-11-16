937bb6896399610db26e59ceb32428c3faa7d9f0
Egidijus Vilimas <eg.vilimas@gmail.com>
2024-11-16 18:41:46 +0100

    Assignment 315C done

--- GetAcademyOppgaver/Assignments/Assignment315C.cs
+++ GetAcademyOppgaver/Assignments/Assignment315C.cs
@@ -5,5 +5,19 @@ public class Assignment315C
     public void Run()
     {
         Console.WriteLine("Oppgaver Assignment 315C");
+        string text = "Terje";
+        Console.WriteLine(text);
+        ReverseString(text);
+        return;
+
+
+        static void ReverseString(string text)
+        {
+            var array = text.ToCharArray();
+            Array.Reverse(array);
+            Console.WriteLine(string.Join("", array));
+            
+        } 
+        
     }
 }
--- GetAcademyOppgaver/Program.cs
+++ GetAcademyOppgaver/Program.cs
@@ -4,8 +4,8 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        var assignment315a = new Assignment315A();
-        assignment315a.Run();
+        // var assignment315a = new Assignment315A();
+        // assignment315a.Run();
 
         var assignment315c = new Assignment315C();
         assignment315c.Run(); 
